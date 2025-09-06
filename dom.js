5bda98f59c742cd64838a0238d220c786f7976da
Luciano Pardo <luciano.pardo999@gmail.com>
2025-09-06 17:24:10 -0300

    2

--- dom.js
+++ dom.js
@@ -1,15 +1,69 @@
-const contenedor = document.querySelector(".flex-contenedor");
+const btnCrearEmpleado = document.getElementById("btnCrearEmpleado");
+const formCrearEmpleado = document.getElementById("formCrearEmpleado");
+const btnCrearMenu = document.getElementById("btnCrearMenu");
+const formCrearMenu = document.getElementById("formCrearMenu");
 
 
-function crearMenu(img, nombre, precio, descripcion) {
-    return [
-        `<img src="${img}"/>`, // Incluyendo la imagen
-        `<h3>${nombre}</h3>`,
-        `<p>$${precio}</p>`,
-        `<p>${descripcion}</p>`
-    ];
-};
+btnCrearEmpleado.addEventListener("click", () => {
+  formCrearEmpleado.classList.toggle("oculto");
+});
 
-const menu = crearMenu("img/hamburguesa.jpg","hamburguesa","100","pan y carne");
+btnCrearMenu.addEventListener("click", () => {
+  formCrearMenu.classList.toggle("oculto");
+});
 
-contenedor.innerHTML = menu.join("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const contenedor = document.querySelector(".flex-contenedor");
+
+
+//function crearMenu(img, nombre, precio, descripcion) {
+//    return [
+//        `<img src="${img}"/>`, // Incluyendo la imagen
+//        `<h3>${nombre}</h3>`,
+//        `<p>$${precio}</p>`,
+//        `<p>${descripcion}</p>`
+//    ];
+//};
+
+//const menu = crearMenu("img/hamburguesa.jpg","hamburguesa","100","pan y carne");
+
+//contenedor.innerHTML = menu.join("");
